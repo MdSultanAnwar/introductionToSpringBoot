9fb2f4316cb39d9254e1bb93096ec7df65855252
Md Sultan Anwar <mdsultananwar@gmail.com>
2025-09-24 04:11:35 +0530

    introductionToSpringBoot.java

---
+++ src/main/java/com/codingshuttle/sultan/week1introduction/introductionToSpringBoot/Apple.java
@@ -0,0 +1,11 @@
+package com.codingshuttle.sultan.week1introduction.introductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+
+    public void eatApple() {
+        System.out.println("Eating apple ");
+    }
+}
--- src/main/java/com/codingshuttle/sultan/week1introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ src/main/java/com/codingshuttle/sultan/week1introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,14 +1,22 @@
 package com.codingshuttle.sultan.week1introduction.introductionToSpringBoot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
-        
-	}
+    @Autowired
+    Apple obj;
 
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args)  throws Exception{
+        obj.eatApple();
+    }
 }
